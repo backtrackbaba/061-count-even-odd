2a7deac8b42804b7fe86f72c0cd60986a126f8f9
backtrackbaba <sai.cosmo@gmail.com>
2017-08-26 10:47:14 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,12 @@
 def solution(list_of_nums):
-    """Enter Code Here"""
+    for i in list_of_nums:
+        if i%2 == 0:
+            d1['EVEN']=d1['EVEN'] + 1
+        else:
+            d1['ODD'] = d1['ODD'] + 1
+    return d1
+
+
+list_of_nums = []
+d1 = {'ODD': 0, 'EVEN': 0}
+solution(list_of_nums)
